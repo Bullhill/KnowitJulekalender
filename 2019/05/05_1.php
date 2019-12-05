51b6130e08ac8cf93f79395371263a9a6c8eb678
Bullhill <ole1000000@hotmail.com>
2019-12-05 09:33:32 +0100

    Luke 05

---
+++ 2019/05/05_1.php
@@ -0,0 +1,21 @@
+<?php
+
+$string = str_split('tMlsioaplnKlflgiruKanliaebeLlkslikkpnerikTasatamkDpsdakeraBeIdaegptnuaKtmteorpuTaTtbtsesOHXxonibmksekaaoaKtrssegnveinRedlkkkroeekVtkekymmlooLnanoKtlstoepHrpeutdynfSneloietbol');
+
+//Step 3
+foreach($string as $no => $char){
+	$stepA[count($string)+$no%3*2-3-$no] = $char;}
+//Step 2
+for($i=0;$i < count($stepA); $i = $i+2){
+	$stepB[$i] = $stepA[$i+1];
+	$stepB[$i+1] = $stepA[$i];}
+//Step 1
+for($i = 0; $i < count($stepB);$i++){
+	$stepC[$i] = $stepB[((count($stepB)/2)+$i)%count($stepB)];}
+
+foreach($stepC as $char){
+	echo $char;}
+echo "\n";
+
+
+?>
