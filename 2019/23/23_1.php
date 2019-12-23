d32dc61358cfdd864363848f43933e86ad0756bf
Bullhill <ole1000000@hotmail.com>
2019-12-23 11:24:32 +0100

    Endret til README

--- 2019/23/23_1.php
+++ 2019/23/23_1.php
@@ -27,15 +27,12 @@ echo "\rSvar: " . $c . "\n";
 
 function is_prime($number)
 {
-    // 1 is not prime
     if ( $number == 1 ) {
         return false;
     }
-    // 2 is the only even prime number
     if ( $number == 2 ) {
         return true;
     }
-    // square root algorithm speeds up testing of bigger prime numbers
     $x = sqrt($number);
     $x = floor($x);
     for ( $i = 2 ; $i <= $x ; ++$i ) {
