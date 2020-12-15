02966ed1351f0643a03ad484cd038706f900767a
Bullhill <ole1000000@hotmail.com>
2020-12-15 09:18:01 +0100

    Luke 15

---
+++ 2020/15/15.php
@@ -0,0 +1,34 @@
+<?php
+
+$ordliste = explode("\n", file_get_contents('wordlist.txt'));
+$testord = explode("\n", file_get_contents('riddles.txt'));
+
+
+foreach($ordliste as $ord)
+{
+	$ordsjekk[$ord] = $ord;
+}
+
+foreach($testord as $test)
+{
+	list($forste, $andre) = explode(', ', $test);
+	
+	foreach($ordsjekk as $ord)
+	{
+		if(isset($ordsjekk[$forste . $ord]) && isset($ordsjekk[$ord . $andre]))
+		{
+			//echo $forste . " - " . $ordsjekk[$forste . $ord] . " - " . $ordsjekk[$ord . $andre] . " - " . $andre . " - " . substr($ordsjekk[$forste . $ord], strlen($forste)) .  "\n";
+			$lim[substr($ordsjekk[$forste . $ord], strlen($forste))] = substr($ordsjekk[$forste . $ord], strlen($forste));
+		}
+	}
+}
+
+$c = 0;
+foreach($lim as $limord)
+{
+	$c += strlen(utf8_decode($limord));
+}
+
+echo "Fasit: " . $c . "\n"
+
+?>
