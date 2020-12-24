39b4c89fce0d9a661dd9f6b25a68e85e744d5d0b
Bullhill <ole1000000@hotmail.com>
2020-12-24 08:47:27 +0100

    Laget film

---
+++ 2020/23/23.video.php
@@ -0,0 +1,115 @@
+<?php
+
+
+
+putenv('GDFONTPATH=' . realpath('.'));
+$font = "arial";
+
+
+$input = explode("\n", utf8_decode(file_get_contents("rap_battle.txt")));
+//$input = array('Lil Niz X: nisse pepperkake hygge','Nizzy G: grøt pepperkake grøt grøt','Lil Niz X: nisse julenisse nisse pepperkake','Nizzy G: julegrøt snø');
+
+
+$grunnordinn = explode("\n", utf8_decode(file_get_contents("basewords.txt")));
+
+foreach($grunnordinn as $ord)
+{
+	preg_match('/(.*) (\d*)/', $ord, $matches);
+	$grunnord[] = $matches[1];
+	$grunnordpoeng[$matches[1]] = $matches[2];
+}
+
+
+
+$totalsum = array('Lil Niz X' => 0, 'Nizzy G' => 0);
+
+$imgno = 0;
+
+foreach($input as $linje)
+{
+	preg_match('/(.*): (.*)/', $linje, $matches);
+	
+	$rapord = explode(' ', $matches[2]);
+	$sanger = $matches[1];
+	
+	$forrigegord = "";
+	$forrigevokaler = -1;
+	$repetisjonsdivisor = 1;
+	$linjeverdi = 0;
+	foreach($rapord as $ord)
+	{
+		foreach($grunnordpoeng as $gord => $poeng)
+		{
+			$pos = strpos($ord, $gord);
+			if($pos !== false)
+			{
+				if(strpos($ord, 'jule') !== false)
+					$julebonus = 2;
+				else
+					$julebonus = 1;
+				
+				
+				$vokaler = tellvokaler($ord);
+				
+				if($forrigevokaler != -1 && $vokaler > $forrigevokaler)
+					$vokalbonus = abs($vokaler - $forrigevokaler) * $julebonus;
+				else
+					$vokalbonus = 0;
+				
+				if($forrigegord == $gord)
+					$repetisjonsdivisor++;
+				else
+					$repetisjonsdivisor = 1;
+				
+				$ordverdi = floor(($poeng+$vokalbonus)/$repetisjonsdivisor);
+				echo $ord . " = (" . $poeng . " + " . $vokalbonus . ") / " . $repetisjonsdivisor . " =  " . $ordverdi . "\n";
+				$forrigevokaler = $vokaler;
+				$forrigegord = $gord;
+				
+				$nizzord = 0;
+				$lilord = 0;
+				if($sanger == 'Nizzy G')
+				{
+					$im = imagecreatefrompng("h-alv.png");
+					$nizzord = $ordverdi+$linjeverdi;
+				}
+				else
+				{
+					$im = imagecreatefrompng("v-alv.png");
+					$lilord = $ordverdi+$linjeverdi;
+				}
+				$black = imagecolorallocate($im, 0, 0, 0);
+				imagettftext($im, 16, 0, 215, 115, $black, $font, $ord);
+				imagettftext($im, 16, 0, 50, 430, $black, $font, ($totalsum['Lil Niz X']+$lilord));
+				imagettftext($im, 16, 0, 50, 460, $black, $font, 'Lil Niz X');
+				imagettftext($im, 16, 0, 330, 430, $black, $font, ($totalsum['Nizzy G']+$nizzord));
+				imagettftext($im, 16, 0, 330, 460, $black, $font, 'Nizzy G');
+				
+				for($i = 0; $i <= 12; $i++)
+				{
+					imagepng($im, "imgs/" . str_pad($imgno, 5, "0", STR_PAD_LEFT) . ".png");
+					$imgno++;
+				}
+				break;
+			}
+		}
+		
+		$linjeverdi += $ordverdi;
+		
+	}
+	echo "Total for linje: " . $linjeverdi . "\n";
+	$totalsum[$sanger] += $linjeverdi;
+}
+
+print_r($totalsum);
+
+function tellvokaler($ord)
+{
+	$vokaler = array('a','e','i','o','u','y','æ','ø','å');
+	$c = 0;
+	foreach($vokaler as $vokal)
+	{
+		$c += substr_count($ord, $vokal);
+	}
+	return $c;
+}
